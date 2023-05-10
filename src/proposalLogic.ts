0825e4af9a60a083eb3def9cb0a042035fc7aedf
Ami Hazbany <ami@orbs.com>
2023-05-10 19:57:51 +0300

    fix single voting power

--- src/proposalLogic.ts
+++ src/proposalLogic.ts
@@ -250,7 +250,7 @@ export async function getSingleVoterPower(clientV4: TonClient4, voter: string, p
 
   else if (strategy == VotingPowerStrategyType.NftCcollection) {
     
-    return allNftItemsHolders[voter].toString() || '0'   
+    return (allNftItemsHolders[voter] || 0).toString()
   }
 
   return '0';
