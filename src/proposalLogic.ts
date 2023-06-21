e2fb8367810dc62c8f1e12220fc6baa1c8ab125d
Ami Hazbany <ami@orbs.com>
2023-06-21 10:27:23 +0300

    bugfix in nft collection strategy

--- src/proposalLogic.ts
+++ src/proposalLogic.ts
@@ -301,7 +301,7 @@ export async function getSingleVoterPower(clientV4: TonClient4, voter: string, p
   }
 
   else if (strategy == VotingPowerStrategyType.NftCcollection) {    
-    return (toNano(allNftItemsHolders[voter] || 0)).toString();
+    return (Number(allNftItemsHolders[voter] || '0')).toString();
   }
 
   else if (strategy == VotingPowerStrategyType.NftCcollection_1Wallet1Vote) {
